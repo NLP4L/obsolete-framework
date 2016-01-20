b1adbc219dc8a2b955236c16a1bf2f093b9ead6a
Koji Sekiguchi <koji.sekiguchi@rondhuit.com>
2016-01-20 12:34:48 +0900

    fix #26: add StopWordsProcessor

--- app/org/nlp4l/framework/builtin/FeaturedProcessors.scala
+++ app/org/nlp4l/framework/builtin/FeaturedProcessors.scala
@@ -63,7 +63,6 @@ class StandardSolrQueryLogProcessor(val pattern: Regex, val separator: String) e
         val value = record.cellList(0).value.asInstanceOf[String]
         value match {
           case pattern(a,b,c,d) => {
-            logger.debug(s"$a $b $c $d")
             val cellDate = Cell("date", a)
             val params = b.split("&")
             val q = params.filter(_.startsWith("q=")).map(a => a.substring(2))
@@ -96,5 +95,82 @@ class StandardSolrQueryLogDictionaryAttributeFactory(settings: Map[String, Strin
       CellAttribute("QTime", CellType.IntType, true, true)
     )
     new DictionaryAttribute("solrQueryLog", list)
+  }
+}
+
+object StopWordsUtil {
+
+  private val logger = Logger(this.getClass)
+
+  def stopwords(file: String, encoding: String, separator: String, columnNum: Int): Set[String] = {
+    if(columnNum < 1) throw new IllegalArgumentException(s"column must be more than zero, but it is set to $columnNum")
+    val ff = new File(file).getAbsoluteFile
+    val f = Source.fromFile(ff, encoding)
+    val commentPat = "#.*"
+    try {
+      f.getLines()
+        .map(_.replaceFirst(commentPat, "").trim)
+        .filter(_.length > 0)
+        .map(a => {
+              val params = a.split(separator)
+              if(params.length < columnNum){
+                logger.info(s"the line '$a' was skipped as it has less than $columnNum columns")
+                ""
+              }
+              else {
+                params(columnNum - 1).trim
+              }
+            })
+        .filter(_.length > 0).toSet
+    }
+    finally {
+      f.close()
+    }
+  }
+}
+
+class StopWordsProcessorFactory(settings: Map[String, String]) extends ProcessorFactory(settings) {
+  override def getInstance: Processor = {
+    val file = getStrParamRequired("file")
+    val encoding = settings.getOrElse("encoding", "UTF-8")
+    val separator = settings.getOrElse("separator", ",")
+    val columnNum = getIntParam("column", 1)
+    val stopwords = StopWordsUtil.stopwords(file, encoding, separator, columnNum)
+    val cellName = getStrParamRequired("cellName")
+    new StopWordsProcessor(stopwords, cellName)
+  }
+}
+
+class StopWordsProcessor(val stopwords: Set[String], val cellName: String) extends Processor {
+
+  private val logger = Logger(this.getClass)
+
+  override def execute(data: Option[Dictionary]): Option[Dictionary] = {
+    if(stopwords.size == 0){
+      logger.warn("there is no available stop words in the file.")
+      data
+    }
+    else{
+      data match {
+        case Some(dic) => {
+          Some(Dictionary(
+            dic.recordList.filterNot(stopRecord(_)
+            )
+          ))
+        }
+        case None => None
+      }
+    }
+  }
+
+  def stopRecord(r: Record): Boolean = {
+    val cell = r.cellMap.get(cellName)
+    if(cell == None){
+      logger.warn(s"there is no cell named '$cellName' in the record $r")
+      false
+    }
+    else{
+      stopwords.contains(cell.get.value.toString)
+    }
   }
 }
